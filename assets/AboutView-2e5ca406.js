8b479162404e5183f8cf62824d3893014aada151
Jana Wernick <jana.wernick@gmail.com>
2023-01-13 14:38:49 +0100

    deploy

--- assets/AboutView-2e5ca406.js
+++
@@ -1,1 +0,0 @@
-import{H as o}from"./HelloWorld-685d6b59.js";import{o as e,c as r}from"./index-7edbc643.js";const m={__name:"AboutView",setup(t){return(a,c)=>(e(),r(o,{msg:"AboutView"}))}};export{m as default};
---
+++ assets/HomeView-3a28a1ed.js
@@ -0,0 +1,1 @@
+import{H as e}from"./HelloWorld-92614a1d.js";import{o,c as r}from"./index-de833639.js";const _={__name:"HomeView",setup(t){return(a,c)=>(o(),r(e,{msg:"HomeView???"}))}};export{_ as default};
