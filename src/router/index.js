f3a55e0967b92942de97951320beb7ecb6e7afb0
Jana Wernick <jana.wernick@gmail.com>
2023-01-13 14:50:26 +0100

    add route for 404s in router

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const HomeView = () => import('../views/HomeView.vue');
 const AboutView = () => import('../views/AboutView.vue');
+const PageNotFoundView = () => import('../views/PageNotFoundView.vue');
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -16,7 +17,12 @@ const router = createRouter({
             path: '/about',
             name: 'about',
             component: AboutView,
-        }
+        },
+        {
+            path: '/:notFound(.*)*',
+            name: 'PageNotFound',
+            component: PageNotFoundView,
+          }
     ]
 });
 
