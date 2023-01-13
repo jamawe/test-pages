396411a1b6ae3cc0db23bd1d0beadf62d221eefb
Jana Wernick <jana.wernick@gmail.com>
2023-01-13 14:44:00 +0100

    deploy

---
+++ assets/AboutView-41a6cb9c.js
@@ -0,0 +1,1 @@
+import{H as o}from"./HelloWorld-48013337.js";import{o as e,c as r}from"./index-2f8a22a7.js";const m={__name:"AboutView",setup(t){return(a,c)=>(e(),r(o,{msg:"AboutView"}))}};export{m as default};
--- assets/AboutView-d4c32fa4.js
+++
@@ -1,1 +0,0 @@
-import{H as o}from"./HelloWorld-92614a1d.js";import{o as e,c as r}from"./index-de833639.js";const m={__name:"AboutView",setup(t){return(a,c)=>(e(),r(o,{msg:"AboutView"}))}};export{m as default};
