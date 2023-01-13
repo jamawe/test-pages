5bc606f8465618cc15ddd05cb3eddc9dad8e7cb0
Jana Wernick <jana.wernick@gmail.com>
2023-01-13 14:26:56 +0100

    add vue router

---
+++ src/router/index.js
@@ -0,0 +1,23 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+const HomeView = () => import('../views/HomeView.vue');
+const AboutView = () => import('../views/AboutView.vue');
+
+const router = createRouter({
+    history: createWebHistory(import.meta.env.BASE_URL),
+
+    routes: [
+        {
+            path: '/',
+            name: 'hello',
+            component: HomeView,
+        },
+        {
+            path: '/about',
+            name: 'about',
+            component: AboutView,
+        }
+    ]
+});
+
+export default router;
